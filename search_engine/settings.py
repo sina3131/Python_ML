221e8edd8ed4721c17006983b7a3d1143e6b5b4b
sina <sina@sinas-MacBook-Pro.local>
2023-06-30 10:28:23 +0200

    sdf

---
+++ search_engine/settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY = "YOUR API KEY HERE"
+SEARCH_ID = "YOUR SEARCH ID or CX"
+COUNTRY = "sv"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+RESULT_COUNT = 20
